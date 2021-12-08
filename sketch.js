7b497db60e5dc24c717cb020e7358f5ac9969023
Matheus Nunes;- <93739577+MaThEuSnnunes@users.noreply.github.com>
2021-12-08 14:40:51 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,33 @@
+var pathimg;
+var boyImg,boy;
+var path;
 
 function preload(){
-  //imagens pré-carregadas
+  pathimg = loadImg ("path.img");
+  boyImg = loadAnimation ("Runner-1.png","Runnner-2.png");
+  
 }
 
 function setup(){
   createCanvas(400,400);
-  //crie sprite aqui
-}
-
-function draw() {
-  background(0);
+ path = createsSprite (200,200)
+ path.addImage (pathimg)
+  path.velocityY = 4; path.scale = 1.2;
+  boy = createSprite(180,340,30,30); 
+  boy.scale=0.08; boy.addAnimation("JakeRunning",boyImg); 
+  leftBoundary=createSprite(0,0,100,800); 
+  leftBoundary.visible = false; 
+  rightBoundary=createSprite(410,0,100,800); 
+  rightBoundary.visible = false;
+} 
+function draw()
+{ background(0); 
+  path.velocityY = 4;
+  boy.x = World.mouseX; 
+  edges= createEdgeSprites();
+  boy.collide(edges[3]); 
+  boy.collide(leftBoundary); 
+  boy.collide(rightBoundary);
+  drawSprites ()
 
 }
